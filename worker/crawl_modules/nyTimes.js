31993a98dad0bd023fe98faf6d632ba6efc3be44
Hack Reactor Students <accounts@hackreactor.com>
2015-11-12 15:27:56 -0800

    Working rss modules..

--- worker/crawl_modules/nyTimes.js
+++ worker/crawl_modules/nyTimes.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var querystring = require("querystring");
 //The url we want is: 'www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
-
+var prettyjson = require('prettyjson');
 
 var qString = {
   searchTerm:"tech", // Search query term. Search is performed on the article body, headline and byline.
@@ -14,22 +14,22 @@ var qString = {
   page:"",//Integer, 0–last set of ten default: 0 - The value of page corresponds to a set of 10 results (it does not indicate the starting number of the result set). For example, page=0 corresponds to records 0-9. To return records 10-19, set page to 1, not 10.
   facet_field:"", //section_name document_type type_of_material source day_of_week
   facet_filter:false,//When set to true, facet counts will respect any applied filters (fq, date range, etc.) in addition to the main query term
-  apiKey:""
-
-
-
+  apiKey:"b5d675362072f08970529a1f7477a856%3A7%3A73435646"
 
 }
 
 
-var options = {
-  host: 'http://api.nytimes.com/svc/search/v2/articlesearch.json?',
-  path: 'q=' + qString.searchTerm + '&fq=news_desk%3A%28%22Technology%22%28&additional%2Dparams=&api-key=' + qString.apiKey
-  // path: querystring.stringify('/svc/search/v2/articlesearch.json?q=' + qString.searchTerm + '&fq=section_name:("Technology")&additional-params=values]&api-key=' + qString.apiKey)
-};
+//http://api.nytimes.com/svc/search/v2/articlesearch.response-format?[q=search term&fq=filter-field:(filter-term)&additional-params=values]&api-key=####
 
-  // console.log(options.path)
-  console.log("QUERY URL: ",options.host+options.path)
+
+
+var options = {
+  host: 'api.nytimes.com',
+  path: '/svc/search/v2/articlesearch.json?q="technology"&fq=source:("The+New+York+Times")&api-key='+qString.apiKey  //TODO:  query vs section name vs source etc needs to be ironed out.
+  // path: '/svc/search/v2/articlesearch.json?q="Technology"&fq=section_name:("Technology")&api-key='+qString.apiKey
+  // path: '/svc/search/v2/articlesearch.json?q="Technology"&section_name:("Technology")&api-key='+qString.apiKey
+};
+console.log(options.path)
 
 
 
@@ -43,16 +43,9 @@ callback = function(response) {
 
   //the whole response has been recieved, so we just print it out here
   response.on('end', function () {
-    console.log(str);
+    console.log(prettyjson.render(str));
   });
 }
 
-// http.request(options, callback).end();
-var test = "http://api.nytimes.com/svc/search/v2/articlesearch.json?format=json&query=smoking&api-key="
-http.get(test+qString.apiKey,function(err,resp){
-  if(err){
-    console.log("Error",err.statusCode)
-  }else{
-    console.log(resp)
-  }
-})
+http.request(options, callback).end();
+
