f053aa2f2e93944184ccef72befc52aec1bfcd56
Hack Reactor Students <accounts@hackreactor.com>
2015-11-11 19:33:59 -0800

    https://github.com/charmingmarmot/dartfeed/issues/25 - Server: Setup

---
+++ TechDesign.md
@@ -0,0 +1,12 @@
+#Backend Overview
+normal connect stuff 
+
+create routers 
+
+set up middleware (including router one)
+
+set up routes with controller handler 
+
+set up auth controller to handle req/res - include auth 
+set up user controller to handle req/res - include lookup of prefs, following, history
+set up articles controller to handle req/res - call model which calls DB 
---
+++ server/server.js
@@ -0,0 +1,15 @@
+//normal connect stuff 
+var express = require('express'); 
+var mongoose = require('mongoose'); 
+
+//TODO: connect to db 
+
+//create routers 
+
+//set up middleware (including router one)
+
+//set up routes with controller handler 
+
+//set up auth controller to handle req/res - include auth 
+//set up user controller to handle req/res - include lookup of prefs, following, h
+//set up articles controller to handle req/res - call model which calls DB 
